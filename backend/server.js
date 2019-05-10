de4f39a569366d6fb5433d3022e1364d33bee404
SeniorWalnut <agafonov@etm-system.ru>
2019-05-10 18:13:53 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express =    require('express');
+const cors 	  =    require('cors');
+const pgp = require('pr-promise')(); 
+
+// Epxress middleware
+express.use(epxress.json());
+express.use(cors());
+
+const server = express();
